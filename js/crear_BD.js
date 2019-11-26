e223f2e8e2f98b8fb967795a62c4619dd0a2a52a
unknown <sergiopedroche1997@gmail.com>
2019-11-26 14:53:31 +0100

    carrito funcional

--- js/crear_BD.js
+++ js/crear_BD.js
@@ -11,3 +11,47 @@ function crearBD() {
         this.result.createObjectStore("Coches", { keyPath: "idCoche", autoIncrement: true });
     }
 }
+
+function añadir_coche(obj) {
+    arrGlo.conn = arrGlo.miBd.indexedDB.open("MiTienda");
+    arrGlo.conn.onsuccess = function() {
+        this.result.transaction("Coches", "readwrite").objectStore("Coches").add(obj);
+    };
+}
+
+function comprobar_si_existe() {
+    arrGlo.conn = arrGlo.miBd.indexedDB.open("MiTienda");
+
+    arrGlo.conn.onsuccess = function() {
+        this.result.transaction("Coches", "readwrite").objectStore("Coches").count().onsuccess = function() {
+            if (this.result > 0) {
+                document.getElementById("formulario").classList.add("oculto");
+                document.getElementById("compras").classList.remove("oculto");
+                meter_coches();
+            }
+        };
+    }
+}
+
+function meter_coches() {
+    arrGlo.conn = arrGlo.miBd.indexedDB.open("MiTienda");
+
+    arrGlo.conn.onsuccess = function() {
+        this.result.transaction("Coches", "readwrite").objectStore("Coches").openCursor().onsuccess = function(event) {
+            var cursor = event.target.result;
+            if (cursor) {
+                console.log(cursor);
+                var div = document.createElement("div");
+                div.setAttribute("data-coche", cursor.key);
+                var imagen = document.createElement("img");
+                imagen.setAttribute("src", cursor.value.imagen);
+                div.append(imagen);
+                div.addEventListener("click", añadir_carrito);
+                document.getElementById("compras").append(div);
+                cursor.continue();
+            } else {
+                console.log('Ya se han metido todos los coches.');
+            }
+        }
+    }
+}
--- js/principal.js
+++ js/principal.js
@@ -4,25 +4,80 @@ window.onload = inciar;
 
 var arrGlo = {
     "miBd": {},
-    "conn": ""
+    "conn": "",
+    "imagen": ""
 };
 
 function inciar() {
     crearBD();
+    añadir_funciones_botones();
+    //cambiar_formulario();
+    comprobar_si_existe()
 }
 
+function añadir_funciones_botones() {
+    document.getElementById("foto").addEventListener("change", guardar_imagen);
+    document.getElementById("crear").addEventListener("click", crear_coche);
+}
 
-function visual() {
-    var file = document.getElementById("foto").files[0],
-        reader = new FileReader();
-    reader.onloadend = function() {
-        var b64 = reader.result;
-        // .replace(/^data:.+;base64,/, '')
-        console.log(b64);
-        var imagen = document.createElement("img");
-        imagen.setAttribute("src", b64);
-        document.body.append(imagen);
+function crear_coche() {
+    var escuderia = document.getElementById("escuderia").value;
+    var descripcion = document.getElementById("descripcion").value;
+    var precio = document.getElementById("precio").value;
+
+    if (escuderia.length > 0 && descripcion.length > 0 && precio > 0 &&
+        arrGlo.imagen != "") {
+        var obj_coche = {
+            "escuderia": escuderia,
+            "descripcion": descripcion,
+            "precio": precio,
+            "imagen": arrGlo.imagen
+        };
+        añadir_coche(obj_coche);
     }
+}
+
+function añadir_carrito() {
+    var longitud = document.getElementById("carrito").childNodes.length - 1;
+
+    var clon = this.cloneNode(true);
+    clon.setAttribute("name", "coche");
+
+    var array_coches = document.getElementsByName("coche");
+    console.log(longitud);
+    var existe = false;
+
+    for (let x = 0; x < longitud; x++) {
+        if (array_coches[x].dataset.coche == clon.dataset.coche) {
+            existe = true;
+        }
+    }
+
+    if (existe == false) {
+        var span = document.createElement("span");
+        span.setAttribute("id", "span-" + clon.dataset.coche);
+        span.innerHTML = 1;
+        clon.append(span);
+        document.getElementById("carrito").appendChild(clon);
+    } else {
+        var cantidad = document.getElementById("span-" + clon.dataset.coche).innerHTML;
+        document.getElementById("span-" + clon.dataset.coche).innerHTML = Number(cantidad) + 1;
+    }
+
+}
+
+function guardar_imagen() {
+    var file = document.getElementById("foto").files[0];
+    var reader = new FileReader();
+
     reader.readAsDataURL(file);
 
+    reader.onloadend = function() {
+        arrGlo.imagen = reader.result;
+        //var b64 = reader.result;
+        // .replace(/^data:.+;base64,/, '')
+        /*var imagen = document.createElement("img");
+        imagen.setAttribute("src", b64);
+        document.body.append(imagen);*/
+    }
 }
